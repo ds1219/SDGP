915ab5d8ddf58695c0cee46ae4c25a9d192e288a
Aathiq <123265489+Aathiq-Ahamed@users.noreply.github.com>
2023-02-11 20:50:54 +0530

    React file Added
    I added React file with Tailwind

---
+++ WebApp/lml-web/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className= "App">
+     <div className="w-8/12 h-screen bg-black">
+      
+
+     </div>
+     <div>
+
+     </div>
+    </div>
+  );
+}
+
+export default App;
