538c01c4e26fd7216f6d1e7488882d996fa69cd0
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-02-12 21:38:20 +0530

    barebones api and test

---
+++ backend/api_test.py
@@ -0,0 +1,15 @@
+import requests
+
+response = requests.get("http://localhost:3669/")
+print(response.json())
+
+response = requests.post(
+    "http://localhost:3669/mark",
+    json={
+        "studentID": "20030496",
+        "questionID": "34",
+        "answer": "Richard Stallman",
+        "sessionID": "testSession",
+    },
+)
+print(response.json())
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Setup and Documentation
+
+## USE BLACK WHEN UPDATING THE PYTHON CODE!
+
+## python modules
+
+`pip install flask requests`
+
+- flask
+- requests
