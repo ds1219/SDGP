49327f89ecb34a965f9be6e31083c091fe752814
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-02-17 11:44:39 +0530

    generate and return session code

--- backend/api_test.py
+++ backend/api_test.py
@@ -1,5 +1,6 @@
 import requests
 
+#
 response = requests.get("http://localhost:3669/")
 print(response.json())
 
@@ -9,7 +10,18 @@ response = requests.post(
         "studentID": "20030496",
         "questionID": "34",
         "answer": "Richard Stallman",
-        # "sessionID": "testSession",
+        "sessionID": "testSession",
+    },
+)
+print(response.json())
+
+response = requests.post(
+    "http://localhost:3669/startSession",
+    json={
+        "lecturerID": "20030496",
+        "time": "34",
+        "date": "Richard Stallman",
+        "moduleCode": "testSession",
     },
 )
 print(response.json())
