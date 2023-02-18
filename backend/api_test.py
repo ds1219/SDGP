8b8ba1d3c41209fceaccf654e513cbbc7bc01d02
Aathiq <123265489+Aathiq-Ahamed@users.noreply.github.com>
2023-02-18 22:31:17 +0530

    Revert "Merge branch 'main' of https://github.com/ds1219/SDGP"
    This reverts commit 9680c085321d3a5424fb6d42efa09d76e5f3b7ce, reversing
    changes made to 03b57f2e2f2b40db7e9b20af52ccef9d257af8cd.

--- backend/api_test.py
+++ backend/api_test.py
@@ -1,6 +1,5 @@
 import requests
 
-#
 response = requests.get("http://localhost:3669/")
 print(response.json())
 
@@ -10,18 +9,7 @@ response = requests.post(
         "studentID": "20030496",
         "questionID": "34",
         "answer": "Richard Stallman",
-        "sessionID": "testSession",
-    },
-)
-print(response.json())
-
-response = requests.post(
-    "http://localhost:3669/startSession",
-    json={
-        "lecturerID": "20030496",
-        "time": "34",
-        "date": "Richard Stallman",
-        "moduleCode": "testSession",
+        # "sessionID": "testSession",
     },
 )
 print(response.json())
