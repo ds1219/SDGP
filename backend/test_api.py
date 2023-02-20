83f82624506ec09b3aef7f833f99e74771cd4abc
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-02-20 14:29:13 +0530

    move testing to pytest

---
+++ backend/test_api.py
@@ -0,0 +1,22 @@
+import requests
+
+ENDPOINT = "http://127.0.0.1:3669"
+
+
+def test_check_api_connection():
+    response = requests.get(f"{ENDPOINT}/")
+    assert response.status_code == 200
+
+
+def test_mark_attendance():
+    input = {
+        "studentID": "20030496",
+        "questionID": "34",
+        "answer": "Richard Stallman",
+        "sessionID": "testSession",
+    }
+
+    response = requests.post(f"{ENDPOINT}/markAttendance", json=input)
+
+    print(response.json())
+    assert response.json() == input
