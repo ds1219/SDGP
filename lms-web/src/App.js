38d5cee8697f64c9e0a3123407c9899865ff17bc
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-02-25 16:51:18 +0530

    fixed merge residue

--- lms-web/src/App.js
+++ lms-web/src/App.js
@@ -10,25 +10,12 @@ import Question from './Component/Quiz';
 
 
 function App() {
-<<<<<<< HEAD
-
-
-=======
->>>>>>> fa2a666911037fb82323b065cc7a1198f6f22b28
   return <Routes>
     <Route path="/" element= {<Login/>} />
     <Route path="/lecturer" element={<FileInput/>}/>
     <Route path='/student' element={<QrScanner/>}/>
     <Route path='/quiz' element={<Question/>}/>
     <Route path='/location' element={<Location/>}/>
-<<<<<<< HEAD
   </Routes>
-
-  
-
-=======
-  </Routes> 
->>>>>>> fa2a666911037fb82323b065cc7a1198f6f22b28
-}
 
 export default App;
