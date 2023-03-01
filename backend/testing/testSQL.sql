bad2ac1ba5fa695e8549500d06825e0e6ba8af5c
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-03-01 15:45:33 +0530

    update db preset query

--- backend/testing/testSQL.sql
+++ backend/testing/testSQL.sql
@@ -1,9 +1,11 @@
 /* Run on Initial DB Setup*/
 CREATE USER 'backend'@'localhost' IDENTIFIED BY 'b3k3nd';
+
+/* run each time*/
+DROP DATABASE `sdgpTest`;
 CREATE DATABASE `sdgpTest`;
 GRANT ALL PRIVILEGES ON sdgpTest.* TO 'backend'@'localhost';
-
-/* Will Run Automatically On Test */
+USE sdgpTest;
 CREATE TABLE lectureSessions (sessionID varchar(5),
                               lecturerID varchar(5),
                               sessionTime time,
@@ -11,4 +13,7 @@ CREATE TABLE lectureSessions (sessionID varchar(5),
                               subjectID text
                              );
 CREATE TABLE lecturers (lecturerID varchar (5), firstName text, lastName text, subjectIDs text, hashedPass text);
-CREATE TABLE students (lecturerID varchar (5), firstName text, lastName text, subjectIDs text, hashedPass text);
+CREATE TABLE students (studentID varchar (5), firstName text, lastName text, subjectIDs text, hashedPass text);
+CREATE TABLE subjects (subjectID varchar (5), subjectName text);
+CREATE TABLE questions (questionID varchar (5), question text, sourceID varchar (5));
+CREATE TABLE Attendance (studentID varchar(5), questionID varchar(5), answer text, pass BOOLEAN);
