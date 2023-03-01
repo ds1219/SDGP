bc065ded48f4b560bf838a878cc50fb412c8926f
Aathiq <123265489+Aathiq-Ahamed@users.noreply.github.com>
2023-03-01 14:14:41 +0530

    restore

--- lms-web/src/App.js
+++ lms-web/src/App.js
@@ -5,11 +5,12 @@ import Login from './Component/Login'
 import Location from './Component/Location';
 import FileInput from './Component/FileInput';
 import{Route,Routes,Link} from "react-router-dom";
-import QrScanner from './Component/QrScanner';
+import QRCodeEx from './Component/QrScanner';
 import Question from './Component/Quiz';
 
 
 function App() {
+<<<<<<< Updated upstream
   return <Routes>
     <Route path="/" element= {<Login/>} />
     <Route path="/lecturer" element={<FileInput/>}/>
@@ -17,6 +18,19 @@ function App() {
     <Route path='/quiz' element={<Question/>}/>
     <Route path='/location' element={<Location/>}/>
   </Routes> 
+=======
+
+
+  return <Routes>
+    <Route path="/" element= {<Login/>} />
+    <Route path="/lecturer" element={<FileInput/>}/>
+    <Route path='/student' element={<QRCodeEx/>}/>
+     <Route path='/quiz' element={<Question/>}/>
+  </Routes>
+
+  
+
+>>>>>>> Stashed changes
 }
 
 export default App;
