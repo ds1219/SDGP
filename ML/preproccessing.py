6a6a0c99fb416c34fda1638d8f7624d718f82a5d
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-03-11 05:26:32 +0530

    'fixes'

--- ML/preproccessing.py
+++ ML/preproccessing.py
@@ -5,10 +5,31 @@ def textPreProcess(text):
     return tokenize.sent_tokenize(text)
 
 def getKeywords(text):
-    keywords = []
-    
-    return(keywords)
+    keywords = [['Facebook', 'IPO', 'January 1m 2012'],
+                ['$5 Billion', '845 million', '2.7 Billion'],
+                ['IPO', 'Zuckerberg', '22%', '57%'],
+                ['$38', '$104 billion'],
+                ['May 16', '25%', 'shares'],
+                ['IPO', '$16 Billion', 'third-largest'],
+                ['market capitalization', '19 Billion'],
+                ['The New York Times', 'advertisers',],
+                ['Jimmy Lee', 'JPMorgan Chase'],
+                ['TechCrunch', 'new revenue streams'],
+                ['May 18', 'Nasdaq', 'technical problems'],
+                ['IPO Price', 'underwriters', '38.23', '$0.23', '$3.82'],
+                ['financial press', 'dissapointment', 'opening'],
+                ['IPO', 'record', 'trading volume'],
+                ['May 25.2012', '$31.91', '16.5%'],
+                ['May 22, 2012', 'regulators', 'Financial Industry Regulatory Authority', 'investigate', 'banks', 'Facebook', 'information', 'clients', 'general public'],
+                ['William Galvin', 'Massachusetts Secretary of State', 'Morgan Stanley'],
+                ['allegations', 'investors', 'lawsuits', 'class action suit', '$2.5 billion', 'losses', 'IPO'],
+                ['Bloomberg', 'retail investors', '$630 Million', 'Facebook', 'stock'],
+                ["Standard & Poor's", 'Facebook', 'S&P 500', 'December 21, 2013'],
+                ['May 2, 2014', "Zuckerberg", 'motto'],
+                ['moto', 'Zuckerberg', 'Buisiness Insider', 'interview']
+                ]
 
+    return(keywords)
 
 def removeWikipedia(text):
     #https://stackoverflow.com/questions/67605758/how-to-match-and-remove-wikipedia-refences-with-python-and-re
--- ML/questionGenerator.py
+++ ML/questionGenerator.py
@@ -7,6 +7,14 @@ question_model = T5ForConditionalGeneration.from_pretrained('./ramsrigouthamg-t5
 question_tokenizer = T5Tokenizer.from_pretrained('./ramsrigouthamg-t5_squad_v1')
 question_model = question_model.to(device)
 
+import nltk
+nltk.download('punkt')
+nltk.download('brown')
+nltk.download('wordnet')
+
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import sent_tokenize
+
 def get_question(context,answer,model,tokenizer):
     text = "context: {} answer: {}".format(context,answer)
     encoding = tokenizer.encode_plus(text,max_length=384, pad_to_max_length=False,truncation=True, return_tensors="pt").to(device)
@@ -46,6 +54,12 @@ def generate(text):
   return QA,answers
 
 if __name__ == '__main__':
+
+  text = """Facebook filed for an initial public offering (IPO) on January 1, 2012.[26] The preliminary prospectus stated that the company sought to raise $5 billion, had 845 million monthly active users, and a website accruing 2.7 billion likes and comments daily.[27] After the IPO, Zuckerberg would retain a 22% ownership share in Facebook and would own 57% of the voting shares.[28]
+Underwriters valued the shares at $38 each, valuing the company at $104 billion, the largest valuation to date for a newly public company.[29] On May 16, one day before the IPO, Facebook announced it would sell 25% more shares than originally planned due to high demand.[30] The IPO raised $16 billion, making it the third-largest in US history (slightly ahead of AT&T Wireless and behind only General Motors and Visa). The stock price left the company with a higher market capitalization than all but a few U.S. corporations—surpassing heavyweights such as Amazon, McDonald's, Disney, and Kraft Foods—and made Zuckerberg's stock worth $19 billion.[31][32] The New York Times stated that the offering overcame questions about Facebook's difficulties in attracting advertisers to transform the company into a "must-own stock". Jimmy Lee of JPMorgan Chase described it as "the next great blue-chip".[31] Writers at TechCrunch, on the other hand, expressed skepticism, stating, "That's a big multiple to live up to, and Facebook will likely need to add bold new revenue streams to justify the mammoth valuation."[33]
+Trading in the stock, which began on May 18, was delayed that day due to technical problems with the Nasdaq exchange.[34] The stock struggled to stay above the IPO price for most of the day, forcing underwriters to buy back shares to support the price.[35] At closing bell, shares were valued at $38.23,[36] only $0.23 above the IPO price and down $3.82 from the opening bell value. The opening was widely described by the financial press as a disappointment.[37] The stock nonetheless set a new record for trading volume of an IPO.[38] On May 25, 2012, the stock ended its first full week of trading at $31.91, a 16.5% decline.[39]
+On May 22, 2012, regulators from Wall Street's Financial Industry Regulatory Authority announced that they had begun to investigate whether banks underwriting Facebook had improperly shared information only with select clients rather than the general public. Massachusetts Secretary of State William Galvin subpoenaed Morgan Stanley over the same issue.[40] The allegations sparked "fury" among some investors and led to the immediate filing of several lawsuits, one of them a class action suit claiming more than $2.5 billion in losses due to the IPO.[41] Bloomberg estimated that retail investors may have lost approximately $630 million on Facebook stock since its debut.[42] Standard & Poor's added Facebook to its S&P 500 index on December 21, 2013.[43]
+On May 2, 2014, Zuckerberg announced that the company would be changing its internal motto from "Move fast and break things" to "Move fast with stable infrastructure".[44][45] The earlier motto had been described as Zuckerberg's "prime directive to his developers and team" in a 2009 interview in Business Insider, in which he also said, "Unless you are breaking stuff, you are not moving fast enough."[46]"""
   questions, answers = generate(text)
 
   for i in range(0, len(answers)):
