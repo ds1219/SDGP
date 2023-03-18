2d78d53a019adb61614d1e1801662381a8819a04
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-03-18 15:18:47 +0530

    fixed pytest global variables & added ordering

--- backend/test_api.py
+++ backend/test_api.py
@@ -1,97 +1,100 @@
 import requests
 from dbFunctions import *
-
-ENDPOINT = "http://127.0.0.1:5000"
-
-STUDENTUSERSESSIONID = ""
-LECTURERUSERSESSIONID = ""
+import pytest
+import unittest
 
 
-def test_check_api_connection():
-    response = requests.get(f"{ENDPOINT}/")
-    assert response.status_code == 200
+class apiTests(unittest.TestCase):
+    ENDPOINT = "http://127.0.0.1:5000"
 
+    studentUserSessionID = ""
+    lectuererUserSessionID = ""
 
-def test_registerLecturer():
-    input = {
-        "firstName": "Rich",
-        "lastName": "Erd",
-        "subjectIDs": "12345-54321",
-        "hashedPass": "qwertyuio",
-        "userType": "lecturer",
-        "email": "richerd@why.no",
-    }
+    @pytest.mark.order(1)
+    def test_check_api_connection(self):
+        response = requests.get(f"{self.ENDPOINT}/")
+        assert response.status_code == 200
 
-    expected = ("richerd@why.no", "Rich", "Erd", "12345-54321", "qwertyuio")
+    @pytest.mark.order(2)
+    def test_registerLecturer(self):
+        input = {
+            "firstName": "Rich",
+            "lastName": "Erd",
+            "subjectIDs": "12345-54321",
+            "hashedPass": "qwertyuio",
+            "userType": "lecturer",
+            "email": "richerd@why.no",
+        }
 
-    response = requests.post(f"{ENDPOINT}/register", json=input)
-    dbRow = get_row_from_table("lecturers", "email", "richerd@why.no")
-    print(dbRow[0])
+        expected = ("richerd@why.no", "Rich", "Erd", "12345-54321", "qwertyuio")
 
-    assert response.status_code == 200 and dbRow[0] == expected
+        response = requests.post(f"{self.ENDPOINT}/register", json=input)
+        dbRow = get_row_from_table("lecturers", "email", "richerd@why.no")
+        print(dbRow[0])
 
+        assert response.status_code == 200 and dbRow[0] == expected
 
-def test_registerStudent():
-    input = {
-        "firstName": "David",
-        "lastName": "Sheen",
-        "subjectIDs": "12345-54321",
-        "hashedPass": "qwertyuio",
-        "userType": "student",
-        "email": "davidsheen@why.brah",
-    }
+    @pytest.mark.order(3)
+    def test_registerStudent(self):
+        input = {
+            "firstName": "David",
+            "lastName": "Sheen",
+            "subjectIDs": "12345-54321",
+            "hashedPass": "qwertyuio",
+            "userType": "student",
+            "email": "davidsheen@why.brah",
+        }
 
-    response = requests.post(f"{ENDPOINT}/register", json=input)
-    assert response.status_code == 200
+        response = requests.post(f"{self.ENDPOINT}/register", json=input)
+        assert response.status_code == 200
 
+    @pytest.mark.order(4)
+    def test_loginStudent(self):
+        input = {
+            "userType": "student",
+            "email": "davidsheen@why.brah",
+            "hashedPass": "qwertyuio",
+        }
 
-def test_loginStudent():
-    input = {
-        "userType": "student",
-        "email": "davidsheen@why.brah",
-        "hashedPass": "qwertyuio",
-    }
+        response = requests.post(f"{self.ENDPOINT}/login", json=input)
+        pytest.studentUserSessionID = response.json()["userSessionKey"]
+        assert response.status_code == 200
 
-    response = requests.post(f"{ENDPOINT}/login", json=input)
-    studentUserSessionID = response.json()["userSessionKey"]
-    assert response.status_code == 200
+    @pytest.mark.order(5)
+    def test_loginLecturer(self):
+        input = {
+            "userType": "lecturer",
+            "hashedPass": "qwertyuio",
+            "email": "richerd@why.no",
+        }
 
+        response = requests.post(f"{self.ENDPOINT}/login", json=input)
+        pytest.lectuererUserSessionID = response.json()["userSessionKey"]
+        assert response.status_code == 200
 
-def test_loginLecturer():
-    input = {
-        "userType": "lecturer",
-        "hashedPass": "qwertyuio",
-        "email": "richerd@why.no",
-    }
+    @pytest.mark.order(6)
+    def test_mark_attendace(self):
+        input = {
+            "studentID": "evobh",
+            "questionID": "34",
+            "answer": "Richard Stallman",
+            "lectureSessionID": "12345",
+            "userSessionID": pytest.studentUserSessionID,
+        }
 
-    response = requests.post(f"{ENDPOINT}/login", json=input)
-    lectuererUserSessionID = response.json()["userSessionKey"]
-    assert response.status_code == 200
+        response = requests.post(f"{self.ENDPOINT}/markAttendance", json=input)
+        assert response.json() == input
 
+    @pytest.mark.order(7)
+    def test_start_session(self):
+        input = {
+            "lecturerID": "sqbyc",
+            "sessionTime": "13:00",
+            "sessionDate": "2003-04-04",
+            "subjectID": "testSession",
+            "questionSource": "Richard and Mary are very good Friends",
+            "userSessionID": pytest.lectuererUserSessionID,
+        }
 
-# def test_mark_attendance():
-def test_mark_attendace():
-    input = {
-        "studentID": "evobh",
-        "questionID": "34",
-        "answer": "Richard Stallman",
-        "lectureSessionID": "12345",
-        "userSessionID": studentUserSessionID,
-    }
-
-    response = requests.post(f"{ENDPOINT}/markAttendance", json=input)
-    assert response.json() == input
-
-
-def test_start_session():
-    input = {
-        "lecturerID": "sqbyc",
-        "sessionTime": "13:00",
-        "sessionDate": "2003-04-04",
-        "subjectID": "testSession",
-        "questionSource": "Richard and Mary are very good Friends",
-        "userSessionID": lectuererUserSessionID,
-    }
-
-    response = requests.post(f"{ENDPOINT}/startSession", json=input)
-    assert len(response.json()["lectureSessionID"]) == 5
+        response = requests.post(f"{self.ENDPOINT}/startSession", json=input)
+        assert len(response.json()["lectureSessionID"]) == 5
