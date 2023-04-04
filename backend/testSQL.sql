aca6981386a7c5edefe5a97f7fd0bc31f45e8358
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-04-04 21:51:01 +0530

    updated pytest fro github actions

--- backend/testSQL.sql
+++ backend/testSQL.sql
@@ -7,10 +7,12 @@ CREATE TABLE lectureSessions (sessionID varchar(5),
                               sessionEnd datetime,
                               subjectID text,
                               questionSource text);
-CREATE TABLE lecturers (email varchar (255), firstName text, lastName text, subjectIDs text, hashedPass text);
-CREATE TABLE students (email varchar (255), firstName text, lastName text, subjectIDs text, hashedPass text);
+CREATE TABLE lecturers (email text, firstName text, lastName text, subjectIDs text, hashedPass text);
+CREATE TABLE students (email text, firstName text, lastName text, subjectIDs text, hashedPass text);
 CREATE TABLE subjects (subjectID varchar (5), subjectName text);
 CREATE TABLE questions (questionID varchar (5), question text, answer text, wrongAnswers text, sessionID varchar (5));
-CREATE TABLE studentAnswers (questionID varchar (5), email varchar (255), result boolean);
-CREATE TABLE attendance (email varchar(5), lectureSessionID varchar(5));
+CREATE TABLE studentAnswers (questionID varchar (5), email text, result boolean);
+CREATE TABLE attendance (email text, lectureSessionID varchar(5));
 CREATE TABLE userSessions (userSessionID varchar(10), expiry datetime);
+CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'testpassword';
+GRANT ALL PRIVILEGES ON sdgptest.* TO 'testuser'@'localhost';
