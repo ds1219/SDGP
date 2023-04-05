cd230cec156cbdd06b30d0eec9440a1f0312c9a1
ds1219 <76910979+ds1219@users.noreply.github.com>
2023-04-05 19:54:52 +0530

    better keyword extractor

--- ML/extracting_keywords.py
+++ ML/extracting_keywords.py
@@ -1,36 +1,10 @@
-import nltk
-
-nltk.download("punkt")
-nltk.download("brown")
-nltk.download("wordnet")
-nltk.download("stopwords")
-from nltk.corpus import stopwords
-import string
-import pke
-import traceback
+"""
+Code has been Adapted From:
+Alto, A. (2015). Answer to ‘Extracting all Nouns from a text file using nltk’. Stack Overflow. Available from https://stackoverflow.com/a/33588238 [Accessed 5 April 2023].
+"""
+from textblob import TextBlob
 
 # keyword extraction
 def get_keywords(content):
-    out = []
-    try:
-        extractor = pke.unsupervised.MultipartiteRank()
-        extractor.load_document(input=content, language="en")
-
-        pos = {"PROPN", "NOUN"}
-
-        stoplist = list(string.punctuation)
-        stoplist += ["-lrb-", "-rrb-", "-lcb-", "-rcb-", "-lsb-", "-rsb-"]
-        stoplist += stopwords.words("english")
-
-        extractor.candidate_selection(pos=pos)
-
-        extractor.candidate_weighting(alpha=1.1, threshold=0.75, method="average")
-        keyphrases = extractor.get_n_best(n=15)
-
-        for val in keyphrases:
-            out.append(val[0])
-    except:
-        out = []
-        traceback.print_exc()
-
-    return out
+    blob = TextBlob(content)
+    return blob.noun_phrases
